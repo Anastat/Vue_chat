75e47de574377f0845de38acaaf0d2b179a8e179
Anastasiia <30463278+Anastat@users.noreply.github.com>
2019-03-05 00:36:33 +0200

    update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const path = require('path');
+//const path = require('path');
 const http = require('http');
 const serveStatic = require('serve-static');
 
@@ -16,6 +16,10 @@ if ( process.env.NODE_ENV !== 'production' ) {
   // Serve any static files
   app.use(serveStatic(__dirname + '/dist'));
 }
+
+app.get(/.*/, function(req, res) {
+  res.sendfile(__dirname + "/dist/index.html");
+});
 
 const port = process.env.PORT || 5000;
 
