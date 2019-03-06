8f4b0a007ca3b0b23d931fcd2554732344a2d915
Anastasiia <30463278+Anastat@users.noreply.github.com>
2019-03-06 19:12:53 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,23 +3,14 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
 const http = require('http');
-//const serveStatic = require('serve-static');
 
 const app = express();
 
 app.use(cors())
 app.use(bodyParser.json())
 
-/*if ( process.env.NODE_ENV !== 'production' ) {
-  require('dotenv').config()
-} else if (process.env.NODE_ENV === 'production') {*/
-  // Serve any static files
-  app.use(express.static(path.join(__dirname + '/dist')));
-//}
 
-/*app.get('/', function (req, res) {
-  res.render(path.join(__dirname + '/dist/index.html'))
-})*/
+app.use(express.static(path.join(__dirname + '/dist')));
 
 const port = process.env.PORT || 5000;
 
