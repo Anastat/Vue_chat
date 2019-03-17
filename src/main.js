a389dbe35924151353f75f2b3ee7afe6ba4058be
Anastasiia <30463278+Anastat@users.noreply.github.com>
2019-03-17 22:51:27 +0200

    vue router added

--- src/main.js
+++ src/main.js
@@ -1,8 +1,23 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router';
+import Home from './components/Home.vue'
+import Login from './components/Login.vue'
+import SignUp from './components/SignUp.vue'
+
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  routes: [
+    {path: '/', component: Home},
+    {path: '/login', component: Login },
+    {path: '/signUp', component: SignUp }
+  ]
+})
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
